ff4261d4a4bcf88112b5155680d6175799c05dc4
Jorge Solano Cordero <alexis12115@gmail.com>
2024-08-14 15:27:17 -0600

    image tests

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import GalleyImage from './components/GalleryImage'
 function App() {
   return (
     <div className='flex gap-4'>
-      <GalleyImage image='bg-[url("2.jpg")]' />
-      <GalleyImage image='bg-[url("1.png")]' />
-      <GalleyImage image='bg-[url("2.jpg")]' />
-      <GalleyImage image='bg-[url("1.png")]' />
-      <GalleyImage image='bg-[url("2.jpg")]' />
+      <GalleyImage image='bg-[url("/src/assets/2.jpg")]' />
+      <GalleyImage image='bg-[url("/src/assets/1.png")]' />
+      <GalleyImage image='bg-[url("/src/assets/2.jpg")]' />
+      <GalleyImage image='bg-[url("/public/1.png")]' />
+      <GalleyImage image='bg-[url("/2.jpg")]' />
     </div>
   )
 }
